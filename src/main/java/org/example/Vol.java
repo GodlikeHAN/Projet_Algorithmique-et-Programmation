b657317a2fe8e2683c3c004b944ee8f6349870ae
gabriel-04-10 <156429999+gabriel-04-10@users.noreply.github.com>
2025-04-11 14:06:57 +0200

    Update Vol

--- src/main/java/org/example/Vol.java
+++ src/main/java/org/example/Vol.java
@@ -1,34 +1,35 @@
 package org.example;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 class Vol {
-    private String numeroVol;
-    private String origine;
-    private String destination;
-    private Date dateHeureDepart;
-    private Date dateHeureArrivee;
+    private int numeroVol;
+    private String[] origine = new String[2];
+    private String[] destination = new String[2];
+    private LocalDateTime dateHeureDepart;
+    private LocalDateTime dateHeureArrivee;
     private String etat;
     private Avion avion;
     private Pilote pilote;
     private List<PersonnelCabine> equipageCabine;
     List<Reservation> reservations;
+    public static ArrayList<Vol> listeVolsPlanifies = new ArrayList<>(); // Liste de vols planifiés
 
-    public String getNumeroVol() { return numeroVol; }
-    public String getOrigine() { return origine; }
-    public String getDestination() { return destination; }
-    public Date getDateHeureDepart() { return dateHeureDepart; }
-    public Date getDateHeureArrivee() { return dateHeureArrivee; }
+    public int getNumeroVol() { return numeroVol; }
+    public String[] getOrigine() { return origine; }
+    public String[] getDestination() { return destination; }
+    public LocalDateTime getDateHeureDepart() { return dateHeureDepart; }
+    public LocalDateTime getDateHeureArrivee() { return dateHeureArrivee; }
     public String getEtat() { return etat; }
     public Avion getAvion() { return avion; }
     public void setAvion(Avion avion) { this.avion = avion; }
 
-    public Vol(String numeroVol, String origine, String destination, Date dateHeureDepart, Date dateHeureArrivee) {
+    public Vol(int numeroVol, String origine, String destination, LocalDateTime dateHeureDepart, LocalDateTime dateHeureArrivee) {
         this.numeroVol = numeroVol;
-        this.origine = origine;
-        this.destination = destination;
+        this.origine[0] = origine;
+        this.destination[0] = destination;
         this.dateHeureDepart = dateHeureDepart;
         this.dateHeureArrivee = dateHeureArrivee;
         this.etat = "En planification";
@@ -48,18 +49,37 @@ class Vol {
         reservations.add(reservation);
     }
 
-    public void planifierVol() {
-        if (avion != null && pilote != null && !equipageCabine.isEmpty()) {
-            this.etat = "Planifié";
+    public static List<Vol> planifierVol(String dateHeureDepart) {
+        System.out.println("=========== Vols pour le " + dateHeureDepart + " ===============");
+        List<Vol> volsDuJour = new ArrayList<>();
+        for (Vol vol : Vol.listeVolsPlanifies) {
+
+            if (vol.dateHeureDepart.toString().startsWith(dateHeureDepart)) {
+                volsDuJour.add(vol);
+            }
+        }
+        return volsDuJour;
+    }
+
+    public static void annulerVol(int numeroVol) {      // BUG : annuler vol 1 annule vol 1 et 2.
+        //this.numeroVol = numeroVol;
+        for (Vol v : listeVolsPlanifies) {
+            if (v.numeroVol == numeroVol) {
+                v.etat = "Annulé";
+                System.out.println("Le vol " + numeroVol + " a été annulé.");
+                break;
+            }
         }
     }
 
-    public void annulerVol() {
-        this.etat = "Annulé";
-        reservations.forEach(r -> r.getPassager().annulerReservation(r.getNumeroReservation()));
-        if (avion != null) {
-            avion.affecterVol(null);
-        }
+    public void modifierVol(String origine, String destination, LocalDateTime dateHeureDepart, LocalDateTime dateHeureArrivee, String etat) {
+        this.origine[0] = origine;
+        this.destination[0] = destination;
+        this.dateHeureDepart = dateHeureDepart;
+        this.dateHeureArrivee = dateHeureArrivee;
+        this.Etat = etat;
+
+        System.out.println("Le vol " + numeroVol + " a été modifié.");
     }
 
 }
